c40f1104e5dac977884dc955d61b78bac414c9dc
tyoo <nurfikridenintyo@gmail.com>
2024-05-13 14:29:03 +0700

    Initial commit

---
+++ Assets/Scripts/Game/EnemyController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Animator animator;
+    private Transform target;
+    [SerializeField]
+    public float speed;
+    [SerializeField]
+    public float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+        
+    }
+
+    private void Awake()
+    {
+        target = GameObject.FindFirstObjectByType<PlayerMovement>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+      
+    }
+
+    public void FollowPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+}
