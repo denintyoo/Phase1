69465e8e2cc69ec2fc564dcca366c4513f88e3bb
tyoo <nurfikridenintyo@gmail.com>
2024-05-25 23:11:32 +0700

    penambahan sword+slash(belum jadi)

--- Assets/Scripts/Game/Player/Sword.cs
+++ Assets/Scripts/Game/Player/Sword.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private GameObject slashAnimPrefab;
+    [SerializeField] private Transform slashAnimSpawnPoint;
+
+    private PlayerControls playerControls;
+    private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
+
+    private GameObject slashAnim;
+
+    private void Awake() {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
+        myAnimator = GetComponent<Animator>();
+        playerControls = new PlayerControls();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable() {
+        playerControls.Enable();
+    }
+
+    void Start()
     {
-        
+        playerControls.Combat.Attack.started += _ => Attack();
+    }
+
+    private void Update() {
+        MouseFollowWithOffset();
+    }
+
+    private void Attack() {
+        myAnimator.SetTrigger("Attack");
+
+        slashAnim = Instantiate(slashAnimPrefab, slashAnimSpawnPoint.position, Quaternion.identity);
+        slashAnim.transform.parent = this.transform.parent;
+    }
+
+    public void SwingUpFlipAnim() {
+        slashAnim.gameObject.transform.rotation = Quaternion.Euler(-180, 0, 0);
+
+        if (playerController.FacingLeft) { 
+            slashAnim.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    public void SwingDownFlipAnim() {
+        slashAnim.gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        if (playerController.FacingLeft)
+        {
+            slashAnim.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    private void MouseFollowWithOffset() {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        if (mousePos.x < playerScreenPoint.x) {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        } else {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
