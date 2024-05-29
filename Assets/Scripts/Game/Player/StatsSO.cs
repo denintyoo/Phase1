391c8c84c8bb028130273f6804b52dd863de50f8
VML8 <92667459+VML8@users.noreply.github.com>
2024-05-29 12:01:04 +0700

    Deleted duplicate for player stats.

--- Assets/Scripts/Game/Player/StatsSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public int Health = 10;
-    public int Attack = 2;
-    public int AttackCooldown = 1;
-    public int Magic = 4;
-}
