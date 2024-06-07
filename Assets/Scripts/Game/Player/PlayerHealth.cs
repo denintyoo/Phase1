878ab85bf1556968e30762ff093a83c60df9cf5a
tyoo <nurfikridenintyo@gmail.com>
2024-06-07 11:34:29 +0700

    Penambahan Health Bar
    Health Bar sudah jalan tinggal menunggu atack dari enemy agar hp berkurang, sisanya sudah semua

--- Assets/Scripts/Game/Player/PlayerHealth.cs
+++ Assets/Scripts/Game/Player/PlayerHealth.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-  public void TakeDamage(int damage)
-    {
-      if (!invincible)
-      {
-        Debug.Log(damage);
-        invincible = true;
-        StartCoroutine(startinvicibletimer());
-      }
+  //public void TakeDamage(int damage)
+    //{
+     // if (!invincible)
+    //  {
+     //   Debug.Log(damage);
+    //    invincible = true;
+       // StartCoroutine(startinvicibletimer());
+      //}
         
+   // }
+
+  //private bool invincible = false;
+  //private IEnumerator startinvicibletimer()
+ // {
+   // yield return new WaitForSeconds(1);
+   // invincible = false;
+ // }
+
+    public int maxHealth = 10;
+    public int currentHealth;
+    public HealthBar healthBar;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
-  private bool invincible = false;
-  private IEnumerator startinvicibletimer()
-  {
-    yield return new WaitForSeconds(1);
-    invincible = false;
-  }
+    private void Update()
+    {
+       
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
 }
